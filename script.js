d899077b56272c093b6ac95f1ac1a06e255df238
Anshul Saha <anshulsaha15@gmail.com>
2022-04-04 18:12:53 +0530

    words.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var words = ['their', 'about', 'would', 'other', 'words', 'could', 'write', 'first', 'water', 'after', 'right', 'think', 'years', 'place', 'sound', 'great', 'found', 'those', 'under', 'might', 'while', 'house', 'world', 'below', 'asked', 'large', 'until', 'along', 'being', 'often', 'earth', 'began', 'since', 'study', 'night', 'light', 'above', 'parts', 'young', 'story', 'point', 'times', 'heard', 'whole', 'white', 'given', 'means', 'music', 'miles', 'thing', 'today', 'later', 'using', 'money', 'lines', 'group', 'among', 'learn', 'space', 'table', 'early', 'short', 'hands', 'black', 'shown', 'front', 'voice', 'kinds', 'makes', 'comes', 'close', 'power', 'lived', 'vowel', 'taken', 'built', 'heart', 'ready', 'quite', 'bring', 'round', 'horse', 'stand', 'birds', 'tried', 'least', 'field', 'whose', 'girls', 'third', 'hours', 'moved', 'plant', 'doing', 'names', 'forms', 'heavy', 'ideas', 'cried', 'begin']
+var words = wordList
 
 var usedWords = []
 if(localStorage.getItem('usedWords') != null){
@@ -49,12 +49,12 @@ let currentInputId = attempt.toString() + currentInput.toString();
 const updateCurrentId = () => {
     currentInputId = attempt.toString() + currentInput.toString();
 }
-let status = "playing";
+let gameStatus = "playing";
 
 document.getElementById(currentInputId).focus();
 document.getElementById(currentInputId).disabled = false;
 window.addEventListener('keydown', (e) => {
-    if (alphabets.includes(e.key.toUpperCase()) && status === "playing") {
+    if (alphabets.includes(e.key.toUpperCase()) && gameStatus === "playing") {
         let input = document.getElementById(currentInputId);
         input.value = e.key.toUpperCase()
         if (currentInput != 5) {
@@ -66,7 +66,7 @@ window.addEventListener('keydown', (e) => {
                 document.getElementById(currentInputId).focus()
             }, 100)
         }
-    } else if (e.keyCode == 8 && status === "playing") { // for backspace
+    } else if (e.keyCode == 8 && gameStatus === "playing") { // for backspace
         e.preventDefault()
         let input = document.getElementById(currentInputId);
         if (currentInput != 1 && input.value == '') {
@@ -82,7 +82,7 @@ window.addEventListener('keydown', (e) => {
             input.value = ''
         }
     } else if (e.keyCode == 13) { // on enter key
-        if (currentInput == 5 && status == "playing" && document.getElementById(currentInputId).value != '') {
+        if (currentInput == 5 && gameStatus == "playing" && document.getElementById(currentInputId).value != '') {
             // currentinput is 5
             // check if the input is correct
             let input1 = document.getElementById(attempt.toString() + '1')
@@ -106,13 +106,13 @@ window.addEventListener('keydown', (e) => {
                         document.getElementById(input.value).style.backgroundColor = 'rgb(22, 225, 110)'
                         input.style.backgroundColor = 'rgb(22, 225, 110)'
                     })
-                    status = "w"
+                    gameStatus = "w"
                     setTimeout(() => {
                         alert('GAME OVER. YOU WON')
                         window.location.reload()
                     }, 100)
                 }
-                if (status != "w") {
+                if (gameStatus != "w") {
                     // check letters of correctWord
                     [1,2,3,4,5].forEach(i => {
                         let input = document.getElementById(attempt.toString() + i.toString())
@@ -132,9 +132,9 @@ window.addEventListener('keydown', (e) => {
                     })
                     document.getElementById(currentInputId).disabled = true
                     if (attempt == 5) {
-                        status = "l"
+                        gameStatus = "l"
                         setTimeout(() => {
-                            alert('GAME OVER. YOU LOST')
+                            alert(`GAME OVER. YOU LOST. The correct word was ${correctWord.toUpperCase()}`)
                             window.location.reload()
                         }, 100)
                     } else {
